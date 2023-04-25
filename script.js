7742ed77d2c3e58f0d8efb79fbcffb28dadb971e
pbsora <128522273+pbsora@users.noreply.github.com>
2023-04-25 03:17:30 -0300

    + Function working

--- script.js
+++ script.js
@@ -11,53 +11,90 @@ const clearBt = document.querySelector(".clear");
 let num1 = 0;
 let num2 = 0;
 let operand = "";
+let total = 0;
+let used = false;
+let done = false;
 
 const numbers = document.getElementsByClassName("number");
 
-/* for (let i = 0; i <= numbers.length - 1; i++) {
-  numbers[i].addEventListener("click", () => {
-    lScreen.innerText += `${numbers[i].innerText}`;
-  });
-}
- */
 for (let num of numbers) {
   numbers[num.innerText].addEventListener("click", () => {
+    /* if (done) clearScreen(), (done = false); */
     lScreen.innerText += `${numbers[num.innerText].innerText}`;
   });
 }
 
-/* document
-  .querySelector(".zero")
-  .addEventListener("click", () => (lScreen.innerText += "0"));
- */
 plusBt.addEventListener("click", () => {
-  num1 = lScreen.innerText;
-  operand = "plus";
-  console.log(num1, operand);
-  uScreen.innerText = lScreen.innerText + " + ";
-  lScreen.innerText = "";
-});
-
-equalBt.addEventListener("click", () => {
   if (!operand) {
-    window.alert("Invalid");
+    num1 = lScreen.innerText;
+    operand = "+";
+    uScreen.innerText = `${num1} ${operand}`;
+    lScreen.innerText = "";
+    console.log("aqui");
   } else {
     num2 = lScreen.innerText;
-    uScreen.innerText += " " + lScreen.innerText + " = ";
-    lScreen.innerText = addNums(num1, num2);
+    if (total == 0) {
+      total += addNums(num1, num2);
+    } else {
+      total = addNums(total, num2);
+    }
+    uScreen.innerText = `${total} ${operand} `;
+    console.log("ou aqui");
+    lScreen.innerText = "";
   }
 });
 
+equalBt.addEventListener("click", () => {
+  if (operand) {
+    if (total) num1 = total;
+    num2 = lScreen.innerText;
+    total = addNums(num1, num2);
+    uScreen.innerText = `${num1} ${operand} ${num2} = `;
+    lScreen.innerText = total;
+    operand = "";
+    done = true;
+    total = 0;
+  }
+});
+
+let operate = (num1, num2, operand) => {
+  switch (operand) {
+    case "+":
+      return addNums(num1, num2);
+      break;
+    case "-":
+      subNums(num1, num2);
+  }
+};
+
 clearBt.addEventListener("click", () => {
-  num1 = 0;
-  num2 = 0;
-  operand = "";
-  lScreen.innerText = "";
-  uScreen.innerText = "";
+  clearScreen();
 });
 
 let addNums = (num1, num2) => {
   return +num1 + +num2;
+};
+
+let subNums = (num1, num2) => {
+  return +num1 - +num2;
+};
+
+let multNums = (num1, num2) => {
+  return +num1 * +num2;
+};
+
+let divideNums = (num1, num2) => {
+  return +num1 / +num2;
+};
+
+let clearScreen = () => {
+  num1 = 0;
+  num2 = 0;
+  operand = "";
+  total = 0;
+  used = false;
+  uScreen.innerText = "";
+  lScreen.innerText = "0";
 };
 
 /* let a = 2;
